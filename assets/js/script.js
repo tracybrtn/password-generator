b48c54a3d485eca33bbee646f64688c4d04c47e4
tracybrtn <tracynburton@gmail.com>
2022-08-25 11:36:45 -0500

    fixed bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,43 +7,42 @@
 var promptInfo = ["lowercase", "uppercase", "numeric", "special"];
 
 
-function generatePassword() {
-  // Define password length
-  var passwordLength = window.prompt("How long would you like your password to be? (min 8 characters, max 128.");
-    if (passwordLength === "" || passwordLength === null) {
-      window.alert("Your password needs to be between 8 and 128 characters. Try again.")
-      return generatePassword()  
-    } else if (passwordLength < 8 || passwordLength > 128) {
-      window.alert("Your password needs to be between 8 and 128 characters. Try again.")
-      return generatePassword() 
-    } else if (passwordLength >= 8 || passwordLength <= 128) {
-        window.alert("Your password will have " + passwordLength + " characters.");
-        console.log("passwordLength is " + passwordLength);
-    } else {
-        window.alert("Your password needs to be between 8 and 128 characters. Try again.")
-        return generatePassword();
-    };
-
-    // Loop through lowercase, uppercase, numeric, and special characters.
-      for (var i = 0; i < promptInfo.length; i++) {
-        window.prompt("Would you like to include " + promptInfo[i] + " characters in your password?");
-          if (passwordCharacters === "yes" || passwordCharacters === "Yes" || passwordCharacters === "YES") {
-            window.alert("Your password WILL contain " + promptInfo[i] + " characters.");
-            console.log("Password WILL contain " + promptInfo[i] + " characters.");
-          } else if (passwordCharacters === "no" || passwordCharacters === "No" || passwordCharacters === "NO") {
-            window.alert("Your password WILL NOT contain " + promptInfo[i] + " characters.");
-            console.log("Password WILL NOT contain " + promptInfo[i] + " characters.");
-          } else {
-            window.alert("You must choose YES or NO. Try again.");
-            return generatePassword();
-          };
-      };
-     };
+var promptGenerator = function() {	
+  // Define password length	
+  var passwordLength = window.prompt("How long would you like your password to be? (min 8 characters, max 128.");	
+    if (passwordLength === "" || passwordLength === null) {	
+      window.alert("Your password needs to be between 8 and 128 characters. Try again.")	
+      return promptGenerator()  	
+    } else if (passwordLength < 8 || passwordLength > 128) {	
+      window.alert("Your password needs to be between 8 and 128 characters. Try again.")	
+      return promptGenerator() 	
+    } else if (passwordLength >= 8 || passwordLength <= 128) {	
+        window.alert("Your password will have " + passwordLength + " characters.");	
+        console.log("passwordLength is " + passwordLength);	
+    } else {	
+        window.alert("Your password needs to be between 8 and 128 characters. Try again.")	
+        return promptGenerator()	
+    };	
+    // Loop through lowercase, uppercase, numeric, and special characters.	
+    for (var i = 0; i < promptInfo.length; i++) {	
+      var promptConfirm = window.prompt("Would you like to include " + promptInfo[i] + " characters in your password?");	
+        if (promptConfirm === "yes" || promptConfirm === "Yes" || promptConfirm === "YES") {	
+          window.alert("Your password WILL contain " + promptInfo[i] + " characters.");	
+          console.log("Password WILL contain " + promptInfo[i] + " characters.");	
+        } else if (promptConfirm === "no" || promptConfirm === "No" || promptConfirm === "NO") {	
+          window.alert("Your password WILL NOT contain " + promptInfo[i] + " characters.");	
+          console.log("Password WILL NOT contain " + promptInfo[i] + " characters.");	
+        } else {	
+          window.alert("You must choose YES or NO. Try again.");	
+          return promptGenerator();	
+        };	
+    };	
+};
  
 
 
 // Connect HTML button with JS and 
-document.querySelector("#generate").addEventListener('click', generatePassword);
+document.querySelector("#generate").addEventListener('click', promptGenerator);
 
 // Calculate password
 
