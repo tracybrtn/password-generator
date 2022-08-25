3a8f844a7c35abb4bbd8dfe53159e1ffb0c6164e
tracybrtn <tracynburton@gmail.com>
2022-08-25 00:33:26 -0500

    included password criteria

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,86 @@
 // Assignment code here
 
 // function to generate a random password 
-// var passwordLength = answer from prompt
+//function to set password length
 
 
 // PASSWORD PROMPTS STARTS HERE
 // Connect HTML button with JS and 
-document.querySelector('#generate').addEventListener('click', generatePassword);
+document.querySelector("#generate").addEventListener('click', generatePassword);
 
-// Add event listener to generate prompt
+
+// Start generating the password
 function generatePassword() {
-  window.prompt("test!");
+  var passwordLength = window.prompt("How long would you like your password to be? (min 8 characters, max 128.");
+    if (passwordLength === "" || passwordLength === null) {
+      window.alert("Your password needs to be between 8 and 128 characters. Try again.")
+      return generatePassword()  
+    } else if (passwordLength < 8 || passwordLength > 128) {
+      window.alert("Your password needs to be between 8 and 128 characters. Try again.")
+      return generatePassword() 
+    } else if (passwordLength >= 8 || passwordLength <= 128) {
+        window.alert("Your password will have " + passwordLength + " characters.");
+        console.log(passwordLength);
+    } else {
+        window.alert("Your password needs to be between 8 and 128 characters. Try again.")
+        return generatePassword()
+      };
+  var passwordLowercase = window.prompt("Would you like to include lowercase characters in your password?");
+    if (passwordLowercase === "yes" || passwordLowercase === "Yes" || passwordLowercase === "YES") {
+      window.alert("Your password WILL contain lowercase characters.");
+      console.log(passwordLowercase);
+    } else if (passwordLowercase === "no" || passwordLowercase === "No" || passwordLowercase === "NO") {
+      window.alert("Your password WILL NOT contain lowercase characters.");
+    } else {
+      window.alert("You must choose YES or NO. Try again.");
+      return generatePassword();
+    };
+  var passwordUppercase = window.prompt("Would you like to include UPPERCASE characters in your password?");
+    if (passwordUppercase === "yes" || passwordUppercase === "Yes" || passwordUppercase === "YES") {
+      window.alert("Your password WILL contain UPPERCASE characters.");
+      console.log(passwordUppercase);
+    } else if (passwordUppercase === "no" || passwordUppercase === "No" || passwordUppercase === "NO") {
+      window.alert("Your password WILL NOT contain UPPERCASE characters.");
+    } else {
+      window.alert("You must choose YES or NO. Try again.");
+      return generatePassword();
+    };
+  var passwordNumeric = window.prompt("Would you like to include numeric characters in your password?");
+    if (passwordNumeric === "yes" || passwordNumeric === "Yes" || passwordNumeric === "YES") {
+      window.alert("Your password WILL contain numeric characters.");
+      console.log(passwordNumeric);
+    } else if (passwordNumeric === "no" || passwordNumeric === "No" || passwordNumeric === "NO") {
+      window.alert("Your password WILL NOT contain numeric characters.");
+    } else {
+      window.alert("You must choose YES or NO. Try again.");
+      return generatePassword();
+    };
+  var passwordSpecial = window.prompt("Would you like to include special characters in your password?");
+    if (passwordSpecial === "yes" || passwordSpecial === "Yes" || passwordSpecial === "YES") {
+      window.alert("Your password WILL contain special characters.");
+      console.log(passwordSpecial);
+    } else if (passwordSpecial === "no" || passwordSpecial === "No" || passwordSpecial === "NO") {
+      window.alert("Your password WILL NOT contain special characters.");
+    } else {
+      window.alert("You must choose YES or NO. Try again.");
+      return generatePassword();
+    };
 };
 
+// Calculate password
 
-// prompts on screen - ask for length, lowercase, uppercase, numeric, special characters
-// data is always validated
-// if no option is selected then restart loop
 
-// When all options are selected, validate data again
 
 // function writePassword
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
 
   passwordText.value = password;
 
