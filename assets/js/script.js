32a1b572491fa2a1afbf65f4a4f69b3767be0f3d
tracybrtn <tracynburton@gmail.com>
2022-08-24 22:23:36 -0500

    pseudocode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,15 @@
 // Assignment code here
 
+// when generate password button is clicked, then series of PROMPTS
+//start loop
+
+// prompts on screen - ask for length, lowercase, uppercase, numeric, special characters
+// data is always validated
+// if no option is selected then restart loop
+
+// When all options are selected, validate data again
+
+// function writePassword
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
